7a8a060451c397db18649e7f3cfdfa820948249e
Jacoby <jacobabraham615@gmail.com>
2025-04-05 01:26:27 +0530

    Updated login page design

--- home/urls.py
+++ home/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.homepage, name='home'),
     path('login/', views.login_view, name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('signup/', views.signup_view, name='signup'),
 ]
